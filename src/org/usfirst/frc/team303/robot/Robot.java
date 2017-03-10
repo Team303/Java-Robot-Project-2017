852d37436cadd5cf4ccfd762837918fca9b4eb44
Team303 <Team303@MB-ROBOT-24291.MOHS.local>
2017-03-10 17:23:37 -0500

    Sync-Olive-2

--- src/org/usfirst/frc/team303/robot/Robot.java
+++ src/org/usfirst/frc/team303/robot/Robot.java
@@ -30,6 +30,7 @@ public class Robot extends IterativeRobot {
 	static boolean autoRunOnce = false;
 	static PowerDistributionPanel pdp;
 	
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
